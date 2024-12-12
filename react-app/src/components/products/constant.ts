48ef43608abb7d94834bcf02c9ef72f16da4a8b5
Tauhid <tauhidahmed716@gmail.com>
2024-12-13 02:36:05 +0600

    more constant add

--- react-app/src/components/products/constant.ts
+++ react-app/src/components/products/constant.ts
@@ -1,6 +1,7 @@
 export const SET_COLOR = "SET_COLOR";
 export const SET_SIZE = "SET_SIZE";
-export const ADD_TO_CART = "ADD_TO_CART";
 export const INCREMENT = "INCREMENT";
 export const DECREMENT = "DECREMENT";
 export const RESET = "RESET";
+export const OPEN_CART = "OPEN_CART";
+export const ADD_TO_CART = "ADD_TO_CART";
