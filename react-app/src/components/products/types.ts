7d532e82718ccad41d2ee75b346a27a6d8d2ab8e
Tauhid <tauhidahmed716@gmail.com>
2024-12-13 21:17:33 +0600

    standalone product types

--- react-app/src/components/products/types.ts
+++ react-app/src/components/products/types.ts
@@ -1,20 +1,57 @@
-export type CartProduct = {
-  id: string;
-  colorId: string;
-  sizeId: string;
-  size: string;
-  price: number;
-  count: number;
-  color: string;
-  image: string;
-};
-
 export type Store = {
   isCartOpen: boolean;
-  cart: CartProduct[];
+  cart: CartItem[];
 };
 
 export type StoreAction = {
   payload?: any;
   type: any;
+};
+
+export type CartActions = {
+  increment: () => void;
+  decrement: () => void;
+  updateCart: () => void;
+  value: number;
+};
+
+export type ColorVariant = {
+  id: string;
+  color: string;
+  image: string;
+  default: boolean;
+};
+
+export type SizeVariant = {
+  id: string;
+  size: string;
+  originalPrice: number;
+  salePrice: number;
+  default: boolean;
+};
+
+export type CartItem = {
+  id: string;
+  cid: string;
+  sid: string;
+  name: string;
+  image: string;
+  colors: ColorVariant[];
+  sizes: SizeVariant[];
+  type: string;
+  details: string;
+  originalPrice: number;
+  salePrice: number;
+  count: number;
+  size: string;
+  color: string;
+  model: string;
+};
+
+export type VariantPayload = {
+  size?: string;
+  image?: string;
+  color?: string;
+  salePrice?: number;
+  originalPrice?: number;
 };
